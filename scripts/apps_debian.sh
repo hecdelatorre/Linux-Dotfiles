5c1dbfaeaf30775ac88b3dfd7e40858c17262bc0
hecdelatorre <hector982015@gmail.com>
2023-04-17 23:07:17 -0600

    zstd

--- scripts/apps_debian.sh
+++ scripts/apps_debian.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install -y gnome-disk-utility gparted git numlockx systemd-timesyncd smplayer audacious cifs-utils rtorrent python3 python3-pip python3-venv python3-tk filezilla fonts-firacode p7zip p7zip-full lhasa lzip lzma lzop lzd xz-utils zutils openvpn tree unace w3m zsh xclip
+sudo apt install -y gnome-disk-utility gparted git numlockx systemd-timesyncd smplayer audacious cifs-utils rtorrent python3 python3-pip python3-venv python3-tk filezilla fonts-firacode p7zip p7zip-full lhasa lzip lzma lzop lzd xz-utils zutils openvpn tree unace w3m zsh xclip zstd
