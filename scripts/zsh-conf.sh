cb2e503553f091b7876f71e4775c47aa3337eae5
user <email@gmail.com>
2023-04-19 23:14:36 -0600

    zsh

--- scripts/zsh-conf.sh
+++ scripts/zsh-conf.sh
@@ -1,25 +1,4 @@
 #/bin/bash
-echo "Please select your preferred prompt:"
-echo "1. Simple"
-echo "2. Common"
-
-read -p "Enter your choice (1 or 2): " choice
-
-case $choice in
-    1)
-        echo "Setting up simple prompt..."
-        echo ' PROMPT="%F{cyan}%1/%f %F{green}❯%f "' > ~/.zsh_prompt
-        ;;
-    2)
-        echo "Downloading and setting up common prompt..."
-        curl -fsSL https://github.com/hecdelatorre/common/raw/master/common.zsh-theme > ~/.zsh_prompt
-        ;;
-    *)
-        echo "Invalid choice. Please enter either 1 or 2."
-        exit 1
-        ;;
-esac
-
 cat > ~/.zshrc << EOF
 autoload -U colors && colors
 
@@ -55,5 +34,7 @@ alias xc='xclip -selection clipboard'
 
 #export PATH="$ PATH:/opt/bin"
 EOF
+
+curl -fsSL https://github.com/hecdelatorre/common/raw/master/common.zsh-theme > ~/.zsh_prompt
 
 chsh -s $(which zsh)
